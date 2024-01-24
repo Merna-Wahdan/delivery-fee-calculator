4261cc65e42c54bc2e2f26e56a9194cb878a74c4
Merna <merna.wahdan@gmail.com>
2024-01-24 16:49:00 +0100

    Add new test

--- src/Calculator.test.tsx
+++ src/Calculator.test.tsx
@@ -2,6 +2,16 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Calculator from './Calculator';
 
 describe("Calculator component", () => {
+    test("The UI of your Calculator component appears or not", () => {
+        render(<Calculator />)
+        expect(screen.getByLabelText(/Cart Value/)).toBeInTheDocument()
+        expect(screen.getByLabelText(/Delivary distance/)).toBeInTheDocument()
+        expect(screen.getByLabelText(/Amount of items/)).toBeInTheDocument()
+        expect(screen.getByLabelText(/Time/)).toBeInTheDocument()
+        expect(screen.getByRole('button', {name: /Calculate Delivary Price/})).toBeInTheDocument()
+        expect(screen.getByRole('button', {name: /Reset/})).toBeInTheDocument()
+        expect(screen.getByText(/Delivary Price: 0/)).toBeInTheDocument()
+    })
     test("User enters Cart Value less than 10, a small order surcharge is added to the delivery price which is equal the difference between the cart value and 10€ ", () => {
         const cartValue = "9"
         const deliveryDistance = "499"
@@ -30,28 +40,6 @@ describe("Calculator component", () => {
 
     })
 
-    test("The UI of your Calculator component appears or not", () => {
-    render(<Calculator />)
-    expect(screen.getByLabelText(/Cart Value/)).toBeInTheDocument()
-    expect(screen.getByLabelText(/Delivary distance/)).toBeInTheDocument()
-    expect(screen.getByLabelText(/Amount of items/)).toBeInTheDocument()
-    expect(screen.getByLabelText(/Time/)).toBeInTheDocument()
-    expect(screen.getByRole('button', {name: /Calculate Delivary Price/})).toBeInTheDocument()
-    })
-
-    test("Renders correctly in the DOM", () => {
-        render(<Calculator />)
-        expect(screen.getByLabelText(/Cart Value/)).toBeInTheDocument()
-        expect(screen.getByLabelText(/Delivary distance/)).toBeInTheDocument()
-        expect(screen.getByLabelText(/Amount of items/)).toBeInTheDocument()
-        expect(screen.getByLabelText(/Time/)).toBeInTheDocument()
-        expect(screen.getByRole('button', {name: /Calculate Delivary Price/})).toBeInTheDocument()
-        expect(screen.getByRole('button', {name: /Calculate Delivary Price/})).toBeInTheDocument()
-        expect(screen.getByRole('button', {name: /Reset/})).toBeInTheDocument()
-        expect(screen.getByText(/Delivary Price: 0/)).toBeInTheDocument()
-    })
-
 })
-
 
 
