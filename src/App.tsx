1cb424ab554844b5981025b839593630a6fdf705
Merna <merna.wahdan@gmail.com>
2024-01-26 09:39:14 +0100

    Add calculator styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Calculator from './Calculator';
+import { Box, Container, Flex } from '@chakra-ui/react';
 
 function App() {
   
-  return <Calculator />
+  return (
+    <>
+      <Box
+        position="fixed"
+        inset="0"
+        bgImage="url('/background.jpg')"
+        bgSize="cover"
+        bgPosition="center"
+        filter="blur(10px)"
+    />
+    <Flex
+      position="fixed"
+      inset="0"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Container
+        position="relative" 
+        maxWidth="xl"
+        p={5}
+        boxShadow="2xl"
+        borderRadius="md"
+        bgColor="rgba(255, 255, 255, 0.8)" 
+        >
+        <Calculator />
+      </Container> 
+    </Flex>
+  </>)
 }
 
 export default App;
