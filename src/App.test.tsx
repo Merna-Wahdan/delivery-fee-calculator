fa009518a4d809cf6faa6d7a4e9a7361adee1973
Merna <merna.wahdan@gmail.com>
2024-01-28 11:56:31 +0100

    Add Intl wrap

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import { IntlProvider } from 'react-intl'
 import App from './App'
+import en from './locales/en.json'
 
 test('renders calcaultor', () => {
-  render(<App />)
+  const useIntlProvider = (component: React.ReactNode): ReturnType<typeof render> => {
+    return render(
+      <IntlProvider locale="en" messages={en}>
+      {component}
+      </IntlProvider>
+    )
+  }
+  useIntlProvider(<App />)
   expect(screen.getByText(/Calculate Delivery Price/)).toBeInTheDocument()
 })
